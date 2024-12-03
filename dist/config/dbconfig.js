27d997dbe003ac52183a0e6330b568eb61f751bb
Fuad Hossain <fuadhossainbk01@gmail.com>
2024-12-03 18:33:35 +0600

    Delete dist directory

--- dist/config/dbconfig.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const promise_1 = __importDefault(require("mysql2/promise"));
-const dotenv_config_1 = require("./dotenv.config");
-const db = promise_1.default.createPool(dotenv_config_1.dbconnfig);
-exports.default = db;
